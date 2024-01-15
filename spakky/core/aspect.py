5ca04865ae06e9eaec52050bec983eaa1b0e3ca1
백세종 <sejong418@icloud.com>
2024-01-15 21:39:28 +0900

    비동기 Aspect 추가

--- spakky/core/aspect.py
+++ spakky/core/aspect.py
@@ -1,5 +1,6 @@
-from typing import Any, TypeVar, Callable, ParamSpec
+from typing import Any, TypeVar, Callable, Awaitable, ParamSpec
 from functools import wraps
+from dataclasses import dataclass
 
 from spakky.core.annotation import Annotation
 
@@ -7,6 +8,7 @@ P = ParamSpec("P")
 R = TypeVar("R")
 
 
+@dataclass
 class Aspect(Annotation):
     """`Aspect` class is made to support Aspect Oriented Programming.\n
     You can override joinpoint such as\n
@@ -45,7 +47,7 @@ class Aspect(Annotation):
         def wrapper(*args: P.args, **kwargs: P.kwargs) -> R:
             self.before(*args, **kwargs)
             try:
-                result: Any = self.around(obj, *args, **kwargs)
+                result: R = self.around(obj, *args, **kwargs)
                 self.after_returning(result)
                 return result
             except Exception as e:
@@ -70,3 +72,75 @@ class Aspect(Annotation):
 
     def around(self, func: Callable[P, R], *args: P.args, **kwargs: P.kwargs) -> R:
         return func(*args, **kwargs)
+
+
+@dataclass
+class AsyncAspect(Annotation):
+    """`AsyncAspect` class is made to support Aspect Oriented Programming.\n
+    You can override joinpoint such as\n
+    [`before`, `after_returning`, `after_raising`, `after`, `around`]\n
+    This is Proxy Wrapper for Callable object.\n
+    Here is a example.\n
+    ```python
+    class AroundAspect(AsyncAspect):
+        async def around(
+            self, func: Callable[P, Awaitable[R]], *args: P.args, **kwargs: P.kwargs
+        ) -> R:
+            assert not args
+            assert kwargs == {"name": "John", "age": 30}
+            result: R = await func(*args, **kwargs)
+            assert result == "John30"
+            return result
+
+
+    @AroundAspect()
+    async def func(name: str, age: int) -> str:
+        return name + str(age)
+
+
+    assert await func(name="John", age=30) == "John30"
+    ```
+    """
+
+    def __call__(self, obj: Callable[P, Awaitable[R]]) -> Callable[P, Awaitable[R]]:
+        """Annotate origin function to a Aspect wrapper.
+
+        Args:
+            obj (Callable[P, Awaitable[R]]): Origin async function to wrapped by Aspect
+
+        Returns:
+            Callable[P, Awaitable[R]]: Wrapped async function
+        """
+        obj = super().__call__(obj)
+
+        @wraps(obj)
+        async def wrapper(*args: P.args, **kwargs: P.kwargs) -> R:
+            await self.before(*args, **kwargs)
+            try:
+                result: R = await self.around(obj, *args, **kwargs)
+                await self.after_returning(result)
+                return result
+            except Exception as e:
+                await self.after_raising(e)
+                raise e from e
+            finally:
+                await self.after()
+
+        return wrapper
+
+    async def before(self, *_args: Any, **_kwargs: Any) -> None:
+        return
+
+    async def after_returning(self, _result: Any) -> None:
+        return
+
+    async def after_raising(self, _error: Exception) -> None:
+        return
+
+    async def after(self) -> None:
+        return
+
+    async def around(
+        self, func: Callable[P, Awaitable[R]], *args: P.args, **kwargs: P.kwargs
+    ) -> R:
+        return await func(*args, **kwargs)
--- tests/unit/core/test_aspect.py
+++ tests/unit/core/test_aspect.py
@@ -1,13 +1,16 @@
 import inspect
-from typing import Any, Callable, cast
-from dataclasses import dataclass
+from uuid import UUID, uuid4
+from typing import Any, Callable, Awaitable, cast
+from datetime import datetime
+from dataclasses import field, dataclass
 
 import pytest
 
-from spakky.core.aspect import Aspect, P, R
+from spakky.core.aspect import Aspect, AsyncAspect, P, R
 
 
 def test_aspect_is_callable() -> None:
+    @dataclass
     class DummyAspect(Aspect):
         ...
 
@@ -20,6 +23,7 @@ def test_aspect_is_callable() -> None:
 
 
 def test_aspect_type_attribute() -> None:
+    @dataclass
     class DummyAspect(Aspect):
         ...
 
@@ -43,6 +47,7 @@ def test_aspect_before_expect_success() -> None:
 
     aspect_called: bool = False
 
+    @dataclass
     class BeforeAspect(Aspect):
         def before(self, *_args: Any, **_kwargs: Any) -> None:
             nonlocal aspect_called
@@ -68,6 +73,7 @@ def test_aspect_after_returning_expect_success() -> None:
     return_value: User | None = None
     raised_exception: Exception | None = None
 
+    @dataclass
     class AfterReturningAspect(Aspect):
         def after_returning(self, _result: Any) -> None:
             nonlocal aspect_returned
@@ -110,6 +116,7 @@ def test_aspect_after_raising_expect_success() -> None:
     return_value: User | None = None
     raised_exception: Exception | None = None
 
+    @dataclass
     class AfterRaisingAspect(Aspect):
         def after_returning(self, _result: Any) -> None:
             nonlocal aspect_returned
@@ -153,6 +160,7 @@ def test_aspect_after_expect_success() -> None:
     return_value: User | None = None
     raised_exception: Exception | None = None
 
+    @dataclass
     class AfterAspect(Aspect):
         def after_returning(self, _result: Any) -> None:
             nonlocal aspect_returned
@@ -186,6 +194,7 @@ def test_aspect_after_expect_success() -> None:
 
 
 def test_aspect_around_expect_success() -> None:
+    @dataclass
     class AroundAspect(Aspect):
         def around(self, func: Callable[P, R], *args: P.args, **kwargs: P.kwargs) -> R:
             assert not args
@@ -203,3 +212,299 @@ def test_aspect_around_expect_success() -> None:
         func("John", 30)
     with pytest.raises(AssertionError):
         func(name="John", age=29)
+
+
+def test_log_aspect() -> None:
+    logs: list[str] = []
+
+    @dataclass
+    class LogAspect(Aspect):
+        request_id: UUID | None = field(init=False, default=None)
+        timestamp: datetime = field(init=False)
+        arguments: tuple[Any, ...] = field(init=False, default_factory=tuple[Any, ...])
+        keywords: dict[str, Any] = field(init=False, default_factory=dict[str, Any])
+
+        def before(self, *_args: Any, **_kwargs: Any) -> None:
+            self.request_id = _kwargs.get("request_id", None)
+            self.timestamp = datetime.now()
+            self.arguments = _args
+            self.keywords = _kwargs
+            return super().before(*_args, **_kwargs)
+
+        def after(self) -> None:
+            nonlocal logs
+            logs.append(
+                f"[{self.request_id}][{self.timestamp}] {self.arguments}, {self.keywords}"
+            )
+            return super().after()
+
+    @LogAspect()
+    def execute(request_id: UUID, name: str, age: int) -> tuple[UUID, str, int]:
+        return request_id, name, age
+
+    uid1: UUID = uuid4()
+    assert execute(request_id=uid1, name="John", age=30) == (uid1, "John", 30)
+    uid2: UUID = uuid4()
+    assert execute(request_id=uid2, name="John", age=30) == (uid2, "John", 30)
+    uid3: UUID = uuid4()
+    assert execute(request_id=uid3, name="John", age=30) == (uid3, "John", 30)
+    uid4: UUID = uuid4()
+    assert execute(request_id=uid4, name="John", age=30) == (uid4, "John", 30)
+
+    assert len(logs) == 4
+
+
+@pytest.mark.asyncio
+async def test_async_aspect_is_coroutine_callable() -> None:
+    @dataclass
+    class DummyAspect(AsyncAspect):
+        ...
+
+    @DummyAspect()
+    async def sample(name: str, age: int) -> str:
+        return name + str(age)
+
+    assert isinstance(sample, Callable)
+    assert inspect.isfunction(sample)
+    assert inspect.iscoroutinefunction(sample)
+
+
+@pytest.mark.asyncio
+async def test_async_aspect_type_attribute() -> None:
+    @dataclass
+    class DummyAspect(AsyncAspect):
+        ...
+
+    @DummyAspect()
+    async def func(name: str, age: int) -> tuple[str, int]:
+        """dummy doc"""
+        return name, age
+
+    assert func.__module__ == "tests.unit.core.test_aspect"
+    assert func.__name__ == "func"
+    assert func.__qualname__ == "test_async_aspect_type_attribute.<locals>.func"
+    assert func.__doc__ == "dummy doc"
+    assert func.__annotations__ == {"name": str, "age": int, "return": tuple[str, int]}
+
+
+@pytest.mark.asyncio
+async def test_async_aspect_before_expect_success() -> None:
+    @dataclass
+    class User:
+        name: str
+        age: int
+
+    aspect_called: bool = False
+
+    @dataclass
+    class BeforeAspect(AsyncAspect):
+        async def before(self, *_args: Any, **_kwargs: Any) -> None:
+            nonlocal aspect_called
+            aspect_called = True
+            return await super().before(*_args, **_kwargs)
+
+    @BeforeAspect()
+    async def func(name: str, age: int) -> User:
+        return User(name=name, age=age)
+
+    assert await func(name="John", age=30) == User("John", 30)
+    assert aspect_called is True
+
+
+@pytest.mark.asyncio
+async def test_async_aspect_after_returning_expect_success() -> None:
+    @dataclass
+    class User:
+        name: str
+        age: int
+
+    aspect_returned: bool = False
+    aspect_raised: bool = False
+    return_value: User | None = None
+    raised_exception: Exception | None = None
+
+    @dataclass
+    class AfterReturningAspect(AsyncAspect):
+        async def after_returning(self, _result: Any) -> None:
+            nonlocal aspect_returned
+            nonlocal return_value
+            aspect_returned = True
+            return_value = cast(User, _result)
+            return await super().after_returning(_result)
+
+        async def after_raising(self, _error: Exception) -> None:
+            nonlocal aspect_raised
+            nonlocal raised_exception
+            aspect_raised = True
+            raised_exception = _error
+            return await super().after_raising(_error)
+
+    @AfterReturningAspect()
+    async def func(name: str, age: int) -> User:
+        return User(name=name, age=age)
+
+    assert aspect_returned is False
+    assert aspect_raised is False
+    assert return_value is None
+    user: User = await func("John", 30)
+    assert user.name == "John"
+    assert user.age == 30
+    assert aspect_returned is True
+    assert aspect_raised is False
+    assert return_value is not None
+    assert return_value == user
+
+
+@pytest.mark.asyncio
+async def test_async_aspect_after_raising_expect_success() -> None:
+    @dataclass
+    class User:
+        name: str
+        age: int
+
+    aspect_returned: bool = False
+    aspect_raised: bool = False
+    return_value: User | None = None
+    raised_exception: Exception | None = None
+
+    @dataclass
+    class AfterRaisingAspect(AsyncAspect):
+        async def after_returning(self, _result: Any) -> None:
+            nonlocal aspect_returned
+            nonlocal return_value
+            aspect_returned = True
+            return_value = cast(User, _result)
+            return await super().after_returning(_result)
+
+        async def after_raising(self, _error: Exception) -> None:
+            nonlocal aspect_raised
+            nonlocal raised_exception
+            aspect_raised = True
+            raised_exception = _error
+            return await super().after_raising(_error)
+
+    @AfterRaisingAspect()
+    async def func(name: str, age: int) -> User:
+        raise RuntimeError("Unexpected Runtime error occurred")
+
+    assert aspect_returned is False
+    assert aspect_raised is False
+    assert return_value is None
+    assert raised_exception is None
+    with pytest.raises(RuntimeError):
+        await func("John", 30)
+    assert aspect_returned is False
+    assert aspect_raised is True
+    assert return_value is None
+    assert raised_exception is not None
+    assert isinstance(raised_exception, RuntimeError)
+
+
+@pytest.mark.asyncio
+async def test_async_aspect_after_expect_success() -> None:
+    @dataclass
+    class User:
+        name: str
+        age: int
+
+    aspect_returned: bool = False
+    aspect_raised: bool = False
+    return_value: User | None = None
+    raised_exception: Exception | None = None
+
+    @dataclass
+    class AfterAspect(AsyncAspect):
+        async def after_returning(self, _result: Any) -> None:
+            nonlocal aspect_returned
+            nonlocal return_value
+            aspect_returned = True
+            return_value = cast(User, _result)
+            return await super().after_returning(_result)
+
+        async def after_raising(self, _error: Exception) -> None:
+            nonlocal aspect_raised
+            nonlocal raised_exception
+            aspect_raised = True
+            raised_exception = _error
+            return await super().after_raising(_error)
+
+    @AfterAspect()
+    async def func(name: str, age: int) -> User:
+        raise RuntimeError("Unexpected Runtime error occurred")
+
+    assert aspect_returned is False
+    assert aspect_raised is False
+    assert return_value is None
+    assert raised_exception is None
+    with pytest.raises(RuntimeError):
+        await func(name="John", age=30)
+    assert aspect_returned is False
+    assert aspect_raised is True
+    assert return_value is None
+    assert raised_exception is not None
+    assert isinstance(raised_exception, RuntimeError)
+
+
+@pytest.mark.asyncio
+async def test_async_aspect_around_expect_success() -> None:
+    @dataclass
+    class AroundAspect(AsyncAspect):
+        async def around(
+            self, func: Callable[P, Awaitable[R]], *args: P.args, **kwargs: P.kwargs
+        ) -> R:
+            assert not args
+            assert kwargs == {"name": "John", "age": 30}
+            result: R = await func(*args, **kwargs)
+            assert result == "John30"
+            return result
+
+    @AroundAspect()
+    async def func(name: str, age: int) -> str:
+        return name + str(age)
+
+    assert await func(name="John", age=30) == "John30"
+    with pytest.raises(AssertionError):
+        await func("John", 30)
+    with pytest.raises(AssertionError):
+        await func(name="John", age=29)
+
+
+@pytest.mark.asyncio
+async def test_async_log_aspect() -> None:
+    logs: list[str] = []
+
+    @dataclass
+    class LogAspect(AsyncAspect):
+        request_id: UUID | None = field(init=False, default=None)
+        timestamp: datetime = field(init=False)
+        arguments: tuple[Any, ...] = field(init=False, default_factory=tuple[Any, ...])
+        keywords: dict[str, Any] = field(init=False, default_factory=dict[str, Any])
+
+        async def before(self, *_args: Any, **_kwargs: Any) -> None:
+            self.request_id = _kwargs.get("request_id", None)
+            self.timestamp = datetime.now()
+            self.arguments = _args
+            self.keywords = _kwargs
+            return await super().before(*_args, **_kwargs)
+
+        async def after(self) -> None:
+            nonlocal logs
+            logs.append(
+                f"[{self.request_id}][{self.timestamp}] {self.arguments}, {self.keywords}"
+            )
+            return await super().after()
+
+    @LogAspect()
+    async def execute(request_id: UUID, name: str, age: int) -> tuple[UUID, str, int]:
+        return request_id, name, age
+
+    uid1: UUID = uuid4()
+    assert await execute(request_id=uid1, name="John", age=30) == (uid1, "John", 30)
+    uid2: UUID = uuid4()
+    assert await execute(request_id=uid2, name="John", age=30) == (uid2, "John", 30)
+    uid3: UUID = uuid4()
+    assert await execute(request_id=uid3, name="John", age=30) == (uid3, "John", 30)
+    uid4: UUID = uuid4()
+    assert await execute(request_id=uid4, name="John", age=30) == (uid4, "John", 30)
+
+    assert len(logs) == 4
