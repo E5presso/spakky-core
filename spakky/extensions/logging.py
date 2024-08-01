197875bdbe23fecc658a0b1233a17ff6b11a4d92
백세종 <sejong418@icloud.com>
2024-08-01 11:18:43 +0900

    feat: 동기식 로깅 Aspect 추가

--- spakky/extensions/logging.py
+++ spakky/extensions/logging.py
@@ -5,16 +5,22 @@ from logging import Logger
 from dataclasses import field, dataclass
 
 from spakky.aop.advice import Around
-from spakky.aop.advisor import IAsyncAdvisor
-from spakky.aop.aspect import AsyncAspect
+from spakky.aop.advisor import IAdvisor, IAsyncAdvisor
+from spakky.aop.aspect import Aspect, AsyncAspect
 from spakky.aop.order import Order
 from spakky.bean.autowired import autowired
 from spakky.core.annotation import FunctionAnnotation
-from spakky.core.types import AsyncFunc
+from spakky.core.types import AsyncFunc, Func
 
 
 @dataclass
 class AsyncLogging(FunctionAnnotation):
+    enable_masking: bool = True
+    masking_keys: list[str] = field(default_factory=lambda: ["secret", "key", "password"])
+
+
+@dataclass
+class Logging(FunctionAnnotation):
     enable_masking: bool = True
     masking_keys: list[str] = field(default_factory=lambda: ["secret", "key", "password"])
 
@@ -51,6 +57,7 @@ class AsyncLoggingAdvisor(IAsyncAdvisor):
             result = await joinpoint(*args, **kwargs)
         except Exception as e:
             end: float = perf_counter()
+            # pylint: disable=line-too-long
             error: str = (
                 f"[{type(self).__name__}] {joinpoint.__qualname__}({_args}{_kwargs}) raised {type(e).__name__} ({end - start:.2f}s)"
             )
@@ -59,6 +66,58 @@ class AsyncLoggingAdvisor(IAsyncAdvisor):
             )
             raise
         end: float = perf_counter()
+        # pylint: disable=line-too-long
+        after: str = (
+            f"[{type(self).__name__}] {joinpoint.__qualname__}({_args}{_kwargs}) -> {result!r} ({end - start:.2f}s)"
+        )
+        self.__logger.info(
+            mask.sub(self.MASKING_TEXT, after) if annotation.enable_masking else after
+        )
+        return result
+
+
+@Order(0)
+@Aspect()
+class LoggingAdvisor(IAdvisor):
+    MASKING_TEXT: ClassVar[str] = r"\2'******'"
+    MASKING_REGEX: ClassVar[str] = (
+        r"((['\"]?(?={keys})[^'\"]*['\"]?[:=]\s*)['\"][^'\"]*['\"])"
+    )
+    __logger: Logger
+
+    @autowired
+    def __init__(self, logger: Logger) -> None:
+        super().__init__()
+        self.__logger = logger
+
+    @Around(Logging.contains)
+    def around(self, joinpoint: Func, *args: Any, **kwargs: Any) -> Any:
+        start: float = perf_counter()
+        annotation: Logging = Logging.single(joinpoint)
+        masking_keys: str = "|".join(annotation.masking_keys)
+        masking_regex: str = self.MASKING_REGEX.format(keys=masking_keys)
+        mask: re.Pattern[str] | None = re.compile(masking_regex)
+        _args: str = ", ".join(f"{arg!r}" for arg in args) if any(args) else ""
+        _kwargs: str = (
+            ", ".join(f"{key}={value!r}" for key, value in kwargs.items())
+            if any(kwargs)
+            else ""
+        )
+
+        try:
+            result = joinpoint(*args, **kwargs)
+        except Exception as e:
+            end: float = perf_counter()
+            # pylint: disable=line-too-long
+            error: str = (
+                f"[{type(self).__name__}] {joinpoint.__qualname__}({_args}{_kwargs}) raised {type(e).__name__} ({end - start:.2f}s)"
+            )
+            self.__logger.error(
+                mask.sub(self.MASKING_TEXT, error) if annotation.enable_masking else error
+            )
+            raise
+        end: float = perf_counter()
+        # pylint: disable=line-too-long
         after: str = (
             f"[{type(self).__name__}] {joinpoint.__qualname__}({_args}{_kwargs}) -> {result!r} ({end - start:.2f}s)"
         )
