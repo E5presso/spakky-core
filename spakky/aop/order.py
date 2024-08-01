e9aeabbaff0e0bcd9d2750ac3e7bd62eb31546e1
백세종 <sejong418@icloud.com>
2024-08-01 11:19:03 +0900

    feat: Aspect Order 지정 시 음수 유효성 검사 추가

--- spakky/aop/order.py
+++ spakky/aop/order.py
@@ -8,5 +8,9 @@ from spakky.core.annotation import ClassAnnotation
 class Order(ClassAnnotation):
     order: int
 
+    def __post_init__(self) -> None:
+        if self.order < 0:
+            raise ValueError("Order cannot be negative")
+
     def __call__(self, obj: AdvisorT | AsyncAdvisorT) -> AdvisorT | AsyncAdvisorT:
         return super().__call__(obj)
