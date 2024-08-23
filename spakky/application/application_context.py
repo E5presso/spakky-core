28098df7c8245a4733a7122092cf8eaaad22ef94
백세종 <sejong418@icloud.com>
2024-08-24 02:19:35 +0900

    feat: 의존성 순환 참조 검출 로직 추가

--- spakky/application/application_context.py
+++ spakky/application/application_context.py
@@ -3,6 +3,7 @@ from uuid import UUID
 from types import ModuleType
 from typing import Callable, cast
 
+from spakky.application.error import SpakkyApplicationError
 from spakky.application.interfaces.container import (
     IPodContainer,
     NoSuchPodError,
@@ -30,7 +31,12 @@ from spakky.pod.pod import Pod, PodType
 from spakky.pod.primary import Primary
 
 
+class CircularDependencyGraphDetectedError(SpakkyApplicationError):
+    message = "Circular dependency detected"
+
+
 class ApplicationContext(IPodContainer, IPodRegistry, IPluginRegistry):
+    __type_name_map: dict[str, type]
     __type_lookup: dict[type, set[type]]  # dict[base: {derived}]
     __pod_lookup: dict[type, UUID]
     __pods: dict[UUID, Pod]
@@ -46,6 +52,7 @@ class ApplicationContext(IPodContainer, IPodRegistry, IPluginRegistry):
         return {type(x) for x in self.__post_processors}
 
     def __init__(self, package: Module | set[Module] | None = None) -> None:
+        self.__type_name_map = {}
         self.__type_lookup = {}
         self.__pod_lookup = {}
         self.__pods = {}
@@ -64,6 +71,7 @@ class ApplicationContext(IPodContainer, IPodRegistry, IPluginRegistry):
             if base_type not in self.__type_lookup:
                 self.__type_lookup[base_type] = set()
             self.__type_lookup[base_type].add(pod.type_)
+        self.__type_name_map[pod.type_.__name__] = pod.type_
         self.__pod_lookup[pod.type_] = pod.id
         self.__pods[pod.id] = pod
 
@@ -84,13 +92,14 @@ class ApplicationContext(IPodContainer, IPodRegistry, IPluginRegistry):
             pod = post_processor.post_process(self, pod)
         return pod
 
-    def __create_pod_instance(self, pod: Pod) -> object:
+    def __create_pod_instance(self, pod: Pod, dependency_hierarchy: list[type]) -> object:
         if pod.scope == Pod.Scope.SINGLETON and pod.id in self.__singleton_cache:
             return self.__singleton_cache[pod.id]
         dependencies: dict[str, object] = {
-            name: self.get(
+            name: self.__get_internal(
                 type_,
                 name,
+                dependency_hierarchy,
             )
             for name, type_ in pod.dependencies.items()
         }
@@ -100,11 +109,27 @@ class ApplicationContext(IPodContainer, IPodRegistry, IPluginRegistry):
             self.__singleton_cache[pod.id] = processed
         return processed
 
-    def get(self, type_: type[ObjectT], name: str | None = None) -> ObjectT:
+    def __get_internal(
+        self,
+        type_: type[ObjectT],
+        name: str | None = None,
+        dependency_hierarchy: list[type] | None = None,
+    ) -> ObjectT:
+        if isinstance(type_, str):  # To support forward references
+            type_ = self.__type_name_map[type_]  # pragma: no cover
+
+        if dependency_hierarchy is None:
+            dependency_hierarchy = []
+        if type_ in dependency_hierarchy:
+            raise CircularDependencyGraphDetectedError(dependency_hierarchy + [type_])
+        dependency_hierarchy.append(type_)
         resolved_type: type = self.__resolve_type(type_, name)
         pod_id: UUID = self.__pod_lookup[resolved_type]
         pod: Pod = self.__pods[pod_id]
-        return cast(ObjectT, self.__create_pod_instance(pod))
+        return cast(ObjectT, self.__create_pod_instance(pod, dependency_hierarchy))
+
+    def get(self, type_: type[ObjectT], name: str | None = None) -> ObjectT:
+        return self.__get_internal(type_, name)
 
     def contains(self, type_: type, name: str | None = None) -> bool:
         try:
@@ -154,4 +179,4 @@ class ApplicationContext(IPodContainer, IPodRegistry, IPluginRegistry):
         for pod in self.__pods.values():
             if Lazy.exists(pod.obj):
                 continue
-            self.__create_pod_instance(pod)
+            self.__create_pod_instance(pod, [])
