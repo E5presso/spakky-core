1c6046a6f31dd2a774c8db109fd4ab65385b768c
백세종 <sejong418@icloud.com>
2024-09-04 04:15:30 +0900

    fix: 불필요한 cancellation_token 인터페이스 제거

--- spakky/task/async_task.py
+++ spakky/task/async_task.py
@@ -2,23 +2,22 @@ from typing import Callable, Awaitable, TypeAlias
 from asyncio import Event, AbstractEventLoop, new_event_loop
 from threading import Thread
 
-from spakky.task.cancellation_token import ICancellationToken
 from spakky.task.error import TaskAlreadyStartedError, TaskNotStartedError
 from spakky.task.interface import ITask
 
-AsyncTaskAction: TypeAlias = Callable[[ICancellationToken], Awaitable[None]]
+AsyncTaskAction: TypeAlias = Callable[[Event], Awaitable[None]]
 
 
 class AsyncTask(ITask):
     __action: AsyncTaskAction
     __event_loop: AbstractEventLoop
-    __cancellation_token: ICancellationToken
+    __cancellation_token: Event
     __thread: Thread
 
     def __init__(
         self,
         action: AsyncTaskAction,
-        cancellation_token: ICancellationToken | None = None,
+        cancellation_token: Event | None = None,
     ) -> None:
         if cancellation_token is None:
             cancellation_token = Event()
