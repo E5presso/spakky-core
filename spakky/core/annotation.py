6116971dbdd04022d0e120bd6190347a67e1dc23
Spakky <sejong418@icloud.com>
2024-10-08 12:07:41 +0900

    fix: pyright type hint 오류 수정

--- spakky/core/annotation.py
+++ spakky/core/annotation.py
@@ -22,7 +22,7 @@ class Annotation(ABC):
 
     @final
     def __set_metadata(self, obj: AnyT) -> AnyT:
-        metadata: dict[type[Self], list[Self]] = self.__get_metadata(obj)
+        metadata: dict[type, list[Self]] = self.__get_metadata(obj)
         for base_type in type(self).mro():
             if base_type not in metadata:
                 metadata[base_type] = []
@@ -32,21 +32,21 @@ class Annotation(ABC):
 
     @final
     @classmethod
-    def __get_metadata(cls, obj: Any) -> dict[type[Self], list[Self]]:
-        metadata: dict[type[Self], list[Self]] = getattr(obj, __ANNOTATION_METADATA__, {})
+    def __get_metadata(cls, obj: Any) -> dict[type, list[Self]]:
+        metadata: dict[type, list[Self]] = getattr(obj, __ANNOTATION_METADATA__, {})
         return metadata
 
     @final
     @classmethod
     def all(cls, obj: Any) -> list[Self]:
-        metadata: dict[type[Self], list[Self]] = cls.__get_metadata(obj)
+        metadata: dict[type, list[Self]] = cls.__get_metadata(obj)
         annotations: list[Self] = metadata.get(cls, [])
         return annotations
 
     @final
     @classmethod
     def get(cls, obj: Any) -> Self:
-        metadata: dict[type[Self], list[Self]] = cls.__get_metadata(obj)
+        metadata: dict[type, list[Self]] = cls.__get_metadata(obj)
         if cls not in metadata:
             raise AnnotationNotFoundError(cls, obj)
         annotations: list[Self] = metadata.get(cls, [])
@@ -57,7 +57,7 @@ class Annotation(ABC):
     @final
     @classmethod
     def get_or_none(cls, obj: Any) -> Self | None:
-        metadata: dict[type[Self], list[Self]] = cls.__get_metadata(obj)
+        metadata: dict[type, list[Self]] = cls.__get_metadata(obj)
         if cls not in metadata:
             return None
         annotations: list[Self] = metadata.get(cls, [])
@@ -68,7 +68,7 @@ class Annotation(ABC):
     @final
     @classmethod
     def get_or_default(cls, obj: Any, default: Self) -> Self:
-        metadata: dict[type[Self], list[Self]] = cls.__get_metadata(obj)
+        metadata: dict[type, list[Self]] = cls.__get_metadata(obj)
         if cls not in metadata:
             return default
         annotations: list[Self] = metadata.get(cls, [])
@@ -79,7 +79,7 @@ class Annotation(ABC):
     @final
     @classmethod
     def exists(cls, obj: Any) -> bool:
-        metadata: dict[type[Self], list[Self]] = cls.__get_metadata(obj)
+        metadata: dict[type, list[Self]] = cls.__get_metadata(obj)
         return cls in metadata
 
 
