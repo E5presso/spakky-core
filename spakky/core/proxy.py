843bb8a9236842a71eaf610af3c8d1f0db4a0b30
Spakky <sejong418@icloud.com>
2025-07-12 22:09:17 +0900

    fix: 동적 프록시 객체의 원본 참조 경로를 수정

--- spakky/core/proxy.py
+++ spakky/core/proxy.py
@@ -1,8 +1,8 @@
 from abc import ABC, abstractmethod
 from functools import wraps
-from inspect import getmembers, iscoroutinefunction, isfunction
-from types import FunctionType, MethodType, new_class
-from typing import Any, Generic, Iterable, Protocol, final, runtime_checkable
+from inspect import iscoroutinefunction, ismethod
+from types import new_class
+from typing import Any, ClassVar, Generic, Iterable, Protocol, runtime_checkable
 
 from spakky.core.constants import DYNAMIC_PROXY_CLASS_NAME_SUFFIX
 from spakky.core.types import AsyncFunc, Func, ObjectT
@@ -13,7 +13,8 @@ class IProxyHandler(Protocol):
     @abstractmethod
     def call(
         self,
-        func: Func,
+        target: object,
+        method: Func,
         *args: Any,
         **kwargs: Any,
     ) -> Any: ...
@@ -21,121 +22,137 @@ class IProxyHandler(Protocol):
     @abstractmethod
     async def call_async(
         self,
-        func: AsyncFunc,
+        target: object,
+        method: AsyncFunc,
         *args: Any,
         **kwargs: Any,
     ) -> Any: ...
 
     @abstractmethod
-    def get(self, name: str) -> Any: ...
+    def get(self, target: object, name: str) -> Any: ...
 
     @abstractmethod
-    def set(self, name: str, value: Any) -> None: ...
+    def set(self, target: object, name: str, value: Any) -> None: ...
 
     @abstractmethod
-    def delete(self, name: str) -> None: ...
-
-    @abstractmethod
-    def proxy_dir(self) -> Iterable[str]: ...
+    def delete(self, target: object, name: str) -> None: ...
 
 
 class AbstractProxyHandler(IProxyHandler, ABC):
-    _target: object
-
-    def __init__(self, target: object) -> None:
-        self._target = target
-
     def call(
         self,
-        func: Func,
+        target: object,
+        method: Func,
         *args: Any,
         **kwargs: Any,
     ) -> Any:
-        return MethodType(func, self._target)(*args, **kwargs)
+        return method(*args, **kwargs)
 
     async def call_async(
         self,
-        func: AsyncFunc,
+        target: object,
+        method: AsyncFunc,
         *args: Any,
         **kwargs: Any,
     ) -> Any:
-        return await MethodType(func, self._target)(*args, **kwargs)
+        return await method(*args, **kwargs)
 
-    def get(self, name: str) -> Any:
-        return getattr(self._target, name)
+    def get(self, target: object, name: str) -> Any:
+        return getattr(target, name)
 
-    def set(self, name: str, value: Any) -> None:
-        return setattr(self._target, name, value)
+    def set(self, target: object, name: str, value: Any) -> None:
+        return setattr(target, name, value)
 
-    def delete(self, name: str) -> None:
-        return delattr(self._target, name)
-
-    @final
-    def proxy_dir(self) -> Iterable[str]:
-        return dir(self._target)
+    def delete(self, target: object, name: str) -> None:
+        return delattr(target, name)
 
 
 class ProxyFactory(Generic[ObjectT]):
-    __type: type[ObjectT]
-    __functions: dict[str, FunctionType]
-    __handler: IProxyHandler
+    ATTRIBUTES_TO_IGNORE: ClassVar[Iterable[str]] = [
+        "__dict__",
+        "__class__",
+        "__weakref__",
+        "__base__",
+        "__bases__",
+        "__mro__",
+        "__subclasses__",
+        "__name__",
+        "__qualname__",
+        "__module__",
+        "__annotations__",
+        "__doc__",
+    ]
+
+    _type: type[ObjectT]
+    _target: ObjectT
+    _handler: IProxyHandler
 
     def __init__(
         self,
-        type_: type[ObjectT],
+        target: ObjectT,
         handler: IProxyHandler,
     ) -> None:
-        self.__type = type_
-        self.__functions = {
-            name: func for name, func in getmembers(self.__type, isfunction)
-        }
-        self.__handler = handler
+        self._type = type(target)
+        self._target = target
+        self._handler = handler
 
     def __proxy_getattribute__(self, name: str) -> Any:
-        if name not in self.__functions:
-            return self.__proxy_getattr__(name)
-        func = self.__functions[name]
-        if iscoroutinefunction(func):
+        value: Any = object.__getattribute__(self._target, name)
+        if ismethod(value):
+            if iscoroutinefunction(value):
 
-            @wraps(func)
-            async def async_wrapper(*args: Any, **kwargs: Any) -> Any:
-                return await self.__proxy_call_async__(func, *args, **kwargs)
+                @wraps(value)
+                async def async_wrapper(*args: Any, **kwargs: Any) -> Any:
+                    return await self.__proxy_call_async__(value, *args, **kwargs)
 
-            return async_wrapper
+                return async_wrapper
 
-        @wraps(func)
-        def wrapper(*args: Any, **kwargs: Any) -> Any:
-            return self.__proxy_call__(func, *args, **kwargs)
+            @wraps(value)
+            def wrapper(*args: Any, **kwargs: Any) -> Any:
+                return self.__proxy_call__(value, *args, **kwargs)
 
-        return wrapper
+            return wrapper
+        if name in self.ATTRIBUTES_TO_IGNORE:
+            return value
+        return self.__proxy_getattr__(name)
 
     def __proxy_call__(self, method: Func, *args: Any, **kwargs: Any) -> Any:
-        return self.__handler.call(func=method, *args, **kwargs)
+        return self._handler.call(
+            target=self._target,
+            method=method,
+            *args,
+            **kwargs,
+        )
 
     async def __proxy_call_async__(
         self, method: AsyncFunc, *args: Any, **kwargs: Any
     ) -> Any:
-        return await self.__handler.call_async(func=method, *args, **kwargs)
+        return await self._handler.call_async(
+            target=self._target,
+            method=method,
+            *args,
+            **kwargs,
+        )
 
     def __proxy_getattr__(self, name: str) -> Any:
-        return self.__handler.get(name=name)
+        return self._handler.get(target=self._target, name=name)
 
     def __proxy_setattr__(self, name: str, value: Any) -> None:
-        return self.__handler.set(name=name, value=value)
+        return self._handler.set(target=self._target, name=name, value=value)
 
     def __proxy_delattr__(self, name: str) -> None:
-        return self.__handler.delete(name=name)
+        return self._handler.delete(target=self._target, name=name)
 
     def __proxy_dir__(self) -> Iterable[str]:
-        return self.__handler.proxy_dir()
+        return sorted(set(dir(self._target) + list(self._type.__dict__.keys())))
 
     def __proxy_init__(self) -> None:
         return
 
     def create(self) -> ObjectT:
         return new_class(
-            name=self.__type.__name__ + DYNAMIC_PROXY_CLASS_NAME_SUFFIX,
-            bases=(self.__type,),
+            name=self._type.__name__ + DYNAMIC_PROXY_CLASS_NAME_SUFFIX,
+            bases=(self._type,),
             exec_body=lambda ns: ns.update(
                 __getattribute__=self.__proxy_getattribute__,
                 __setattr__=self.__proxy_setattr__,
